6e080037a3e0cb1c2cdc7ce7705d297852e16002
Anton Auoja <anton.auoja@tactel.se>
2016-01-11 14:06:34 +0100

    Added animation

--- demo/script.js
+++ demo/script.js
@@ -2,7 +2,7 @@ function startDemo() {
 
     var canvas = document.getElementById("canvas");
 
-    var kaleidoscope = new Kaleidoscope(canvas, 600, 20, 40, 20, 10);
+    var kaleidoscope = new Kaleidoscope(canvas, 600, 10, 40, 20, 10);
     kaleidoscope.render();
 
 }
--- kaleidoscope.js
+++ kaleidoscope.js
@@ -12,7 +12,11 @@ var Kaleidoscope = function(canvas, size, segments, triangles, circles, rectangl
     var segmentWidth = segmentCanvas.width = radius * Math.sqrt(2 - 2 * Math.cos(angle));
     var segmentHeight = Math.sqrt(radius * radius - (segmentWidth * segmentWidth) / 4);
 
-    function renderSegment() {
+    var entities = [];
+
+    var frame = 0;
+
+    function setupSegment() {
         segmentCtx.moveTo(0, 0);
         segmentCtx.lineTo(segmentWidth, 0);
         segmentCtx.lineTo(segmentWidth * 0.5, segmentHeight);
@@ -20,97 +24,197 @@ var Kaleidoscope = function(canvas, size, segments, triangles, circles, rectangl
         segmentCtx.clip();
 
         var i = 0;
+
         for (i = 0; i < triangles; i++) {
-            drawTriangle();
+            entities.push(new Triangle(segmentWidth, segmentHeight));
         }
 
         for (i = 0; i < circles; i++) {
-            drawCircle();
+            entities.push(new Circle(segmentWidth, segmentHeight));
         }
 
         for (i = 0; i < rectangles; i++) {
-            drawRect();
+            entities.push(new Rectangle(segmentWidth, segmentHeight));
         }
 
+        for (var k, l, m = entities.length; m; k = Math.floor(Math.random() * m), l = entities[--m], entities[m] = entities[k], entities[k] = l);
     }
 
-    function getRandomFillColor() {
-        var color = {
-            r: Math.round(Math.random() * 255),
-            g: Math.round(Math.random() * 255),
-            b: Math.round(Math.random() * 255),
-            a: 0.3 + Math.random() * 0.2
-        };
-        return "rgba(" + color.r + ", " + color.g + ", " + color.b + ", " + color.a + ")";
-    }
+    function renderSegment() {
+        segmentCtx.fillStyle = "black";
+        segmentCtx.fillRect(0, 0, segmentCanvas.width, segmentCanvas.height);
 
-    function drawTriangle() {
-        var startX = Math.round(Math.random() * segmentWidth);
-        var startY = Math.round(Math.random() * segmentHeight);
-
-        function getDelta() {
-            var delta = 0.2 + Math.random() * 0.3;
-            return delta - Math.random() * (2 * delta);
+        for (var i = 0; i < entities.length; i++) {
+            entities[i].draw(segmentCtx, frame);
         }
-
-        segmentCtx.beginPath()
-        segmentCtx.fillStyle = getRandomFillColor();
-        segmentCtx.moveTo(startX, startY);
-        for ( var i = 0; i < 2; i++) {
-            segmentCtx.lineTo(startX + segmentWidth * getDelta(), startY + segmentHeight * getDelta());
-        }
-        segmentCtx.closePath();
-        segmentCtx.fill();
-    }
-
-    function drawCircle() {
-        var startX = Math.round(Math.random() * segmentWidth);
-        var startY = Math.round(Math.random() * segmentHeight);
-        var delta = 0.1 + Math.random() * 0.3;
-
-        segmentCtx.beginPath()
-        segmentCtx.fillStyle = getRandomFillColor();
-        segmentCtx.arc(startX, startY, segmentWidth * delta, 0, 2 * Math.PI);
-        segmentCtx.closePath();
-        segmentCtx.fill();
-    }
-
-    function drawRect() {
-        var startX = Math.round(Math.random() * segmentWidth);
-        var startY = Math.round(Math.random() * segmentHeight);
-
-        function getDelta() {
-            return delta = 0.1 + Math.random() * 0.3;
-        }
-
-        segmentCtx.fillStyle = getRandomFillColor();
-        segmentCtx.fillRect(startX, startY, segmentWidth * getDelta(), segmentHeight * getDelta());
     }
 
     function renderKaleidoscope() {
         ctx.fillStyle = "black";
         ctx.fillRect(0, 0, width, height);
-        for (var i = 0; i < segments; i+=2) {
+
+        var i = 0;
+        for (i = 0; i < segments; i += 2) {
             ctx.translate(0.5 * width, 0.5 * height);
             ctx.rotate(angle * i);
             ctx.translate(-0.5 * width, -0.5 * height);
-            ctx.drawImage(segmentCanvas, 0.5 * (width - segmentWidth), (radius - segmentHeight));
+            ctx.drawImage(segmentCanvas, 0.5 * (width - segmentWidth), (radius - segmentHeight) + 3);
             ctx.setTransform(1, 0, 0, 1, 0, 0);
         }
 
-        for (var i = 1; i < segments; i+=2) {
+        for (i = 1; i < segments; i += 2) {
             ctx.scale(-1, 1);
             ctx.translate(-0.5 * width, 0.5 * height);
             ctx.rotate(angle * -i);
             ctx.translate(0.5 * width, -0.5 * height);
-            ctx.drawImage(segmentCanvas, -0.5 * (width - segmentWidth) - segmentWidth, (radius - segmentHeight));
+            ctx.drawImage(segmentCanvas, -0.5 * (width - segmentWidth) - segmentWidth, (radius - segmentHeight) + 3);
             ctx.setTransform(1, 0, 0, 1, 0, 0);
         }
+
+        // ctx.drawImage(segmentCanvas, 0.5 * (width - segmentWidth), (radius - segmentHeight));
+    }
+
+    function renderFrame() {
+        renderSegment();
+        renderKaleidoscope();
     }
 
     this.render = function() {
-        renderSegment();
-        renderKaleidoscope();
+        setupSegment();
+        setInterval(function() {
+            frame++;
+            if (frame >= 360) {
+                frame = 0;
+            }
+            renderFrame();
+        }, 16);
     };
     return this;
+};
+
+var Entity = function(width, height) {
+    this.center = {
+        x: Math.round(Math.random() * width),
+        y: Math.round(Math.random() * height)
+    };
+    this.width = width;
+    this.height = height;
+    this.direction = Math.random() < 0.5 ? -1 : 1;
+    this.rate = 0.5 + 0.5 * Math.random();
+
+    this.color = {
+        r: Math.round(Math.random() * 255),
+        g: Math.round(Math.random() * 255),
+        b: Math.round(Math.random() * 255),
+        a: 0.6 + Math.random() * 0.2
+    };
+
+    this.counter = 0;
+};
+
+Entity.prototype.getFillColor = function() {
+    return "rgba(" + this.color.r + ", " + this.color.g + ", " + this.color.b + ", " + this.color.a + ")";
+};
+
+Entity.prototype.rotateContext = function(context) {
+    this.counter++;
+
+    if (this.counter * this.rate >= 360) {
+        this.counter = 0;
+    }
+
+    var angle = this.direction * this.counter * this.rate * Math.PI/180;
+
+    context.translate(this.center.x, this.center.y);
+    context.rotate(angle);
+    context.translate(-this.center.x, -this.center.y);
+};
+
+var Circle = function(width, height) {
+    Entity.call(this, width, height);
+
+    this.x = Math.round(Math.random() * width);
+    this.y = Math.round(Math.random() * height);
+    this.radius = Math.min(width, height) * (0.1 + Math.random() * 0.2);
+};
+
+Circle.prototype = Object.create(Entity.prototype);
+
+Circle.prototype.draw = function(context, frame) {
+    this.rotateContext(context);
+
+    context.beginPath();
+    context.fillStyle = this.getFillColor();
+    context.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+    context.closePath();
+    context.fill();
+
+    context.setTransform(1, 0, 0, 1, 0, 0);
+};
+
+var Triangle = function(width, height) {
+    Entity.call(this, width, height);
+
+    var deltaX = getDelta();
+    var deltaY = getDelta();
+
+    this.p1 = {
+        x: Math.round(Math.random() * width),
+        y: Math.round(Math.random() * height)
+    };
+
+    this.p2 = {
+        x: this.p1.x + width * deltaX,
+        y: this.p1.y + height * deltaY
+    };
+
+    this.p3 = {
+        x: this.p1.x - width * deltaX,
+        y: this.p1.y + height * deltaY
+    };
+
+    function getDelta() {
+        return 0.1 + Math.random() * 0.1;
+    }
+};
+
+Triangle.prototype = Object.create(Entity.prototype);
+
+Triangle.prototype.draw = function(context, frame) {
+    this.rotateContext(context);
+
+    context.beginPath();
+    context.fillStyle = this.getFillColor();
+    context.moveTo(this.p1.x, this.p1.y);
+    context.lineTo(this.p2.x, this.p2.y);
+    context.lineTo(this.p3.x, this.p3.y);
+    context.closePath();
+    context.fill();
+
+    context.setTransform(1, 0, 0, 1, 0, 0);
+};
+
+var Rectangle = function(width, height) {
+    Entity.call(this, width, height);
+
+    this.x = Math.round(Math.random() * width);
+    this.y = Math.round(Math.random() * height);
+    this.width = width * getDelta();
+    this.height = height * getDelta();
+
+    function getDelta() {
+        return 0.1 + Math.random() * 0.2;
+    }
+};
+
+Rectangle.prototype = Object.create(Entity.prototype);
+
+Rectangle.prototype.draw = function(context, frame) {
+    this.rotateContext(context);
+
+    context.fillStyle = this.getFillColor();
+    context.fillRect(this.x, this.y, this.width, this.height);
+
+    context.setTransform(1, 0, 0, 1, 0, 0);
+
 };
